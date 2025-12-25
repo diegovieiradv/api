d50cc469b8b4b7f5745676e3e5c958a67d9d713e
Diego Vieira <dvdiego87@gmail.com>
2025-12-25 04:15:44 -0300

    adicionei a classe controller de medico

---
+++ src/test/java/med/fisio/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.fisio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
