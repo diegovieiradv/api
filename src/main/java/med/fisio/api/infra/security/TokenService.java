69a16b1ee408ec3cf15c45558b983e41c30d3c47
Diego Vieira <dvdiego87@gmail.com>
2026-01-08 04:39:36 -0300

    adicionei os metodos para login e requisicoes de usuarios

--- src/main/java/med/fisio/api/infra/security/TokenService.java
+++ src/main/java/med/fisio/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.fisio.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,19 @@ public class TokenService {
 
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API med-fisio")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT invalido ou expirado");
         }
     }
 
